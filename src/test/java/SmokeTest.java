b619adc51cf123f4f70e98501fc3e375d087f938
babu <babu@babu-PC>
2021-12-15 17:16:53 +0530

    again chnaged

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -6,7 +6,7 @@ public class SmokeTest {
 	@Test
 	public void smoketest()
 	{
-		System.out.println("smoke test");
+		System.out.println("smoke test and reg");
 		System.out.println("changed");
 		System.out.println("program updated by sony");
 		System.out.println("program updated by sony");
