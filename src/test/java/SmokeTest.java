ae77a1a3840859679118cfaec6bc1a312afe64f5
sonys <sonys@DESKTOP-LC8G8L6>
2021-12-15 17:17:13 +0530

    change

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -6,8 +6,8 @@ public class SmokeTest {
 	@Test
 	public void smoketest()
 	{
-		System.out.println("smoke test");
-		System.out.println("changed");
+		
+		//System.out.println("changed");
 		System.out.println("program updated by sony");
 
 		System.out.println("done changes");
